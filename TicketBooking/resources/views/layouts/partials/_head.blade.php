eb9cdb782248ac4771f2c49a9f70a294be96fea8
Azizbek <bekmurodov.a@mail.ru>
2019-05-04 10:54:42 +0500

    updated Laravel

---
+++ TicketBooking/resources/views/layouts/partials/_head.blade.php
@@ -0,0 +1,217 @@
+<div class="topnav" id="myTopnav">
+ 	<a href="#home" class="active">
+		<!--header-->
+		<div class="header">
+			<div class="container" style="height: 55px;">
+				<div class="row">
+					<div class="col">
+						<div class="row">
+							<div class="col ">
+									<ul style="padding-top: 12px;">
+										<li>	
+											<button class="change"><i class="fa fa-angle-left"></i></button>
+										</li>
+										<li>	
+											<button class="change"><i class="fa fa-angle-right"></i></button>
+										</li>
+										<li>
+											<p id="match" style="color:#9b9b9b;padding:5px;"><span id="content-data" style="color:#d8cf50">April 15,2018</span>\\
+												<span id="content-club" style="color:white">Atletico VS Real Madrid</span>
+											</p>
+										</li>
+									</ul>
+								
+							</div>
+							<!--end col-->
+						</div>
+					</div>
+					<!--end col-1-->
+					<div class="col-3 ">
+						<div class="trapezoid">
+							
+						</div>
+					</div>
+					<div class="col">
+						<span style="float:right;padding-top:12px;">
+							<ul>
+								<li style="border-right: 1px solid #35363c;padding:0 10px;">	
+									<button style="font-size: 14px;width: 40px">En<i class="fa fa-angle-down pl-1"></i></button>
+								</li>
+								<li style="border-right: 1px solid #35363c;padding:0 10px;">	
+									<i style="color:#d8cf50;font-size: 20px;" class="fa fa-shopping-cart"></i>
+									<span style="background-color: #35ad79;padding:2px 6px;font-weight: bold;border-radius: 50%;color:white;font-size: 12px">2</span>
+								</li>
+								<li style="padding:0 10px;">
+									<i style="color:#d8cf50;font-size: 20px;margin-right:5px;" class="fa fa-sign-in"></i>
+									<span style="color:white">Your acount</span>
+									
+								</li>
+							</ul>
+						</span>
+					</div>
+					<!--end col-->
+				</div>
+				<!--end main row-->
+			</div>
+			<!--end header-->
+			<div style="background: #35363c;height: 125px;">
+				<div class="container">
+					<div class="row brand">
+						<div class="col-5">
+							<ul>
+								<li><img src="Images/partners-1-inverse-75x42.png"></li>
+								<li><img src="Images/nike.png"></li>
+								<li><img src="Images/adidas.png"></li>
+							</ul>
+						</div>
+						<div class="col">
+							<img style="position: absolute;top: -30px;padding-left: 30px;"  src="Images/logo-soccer-default-129x129.png">		
+						</div>
+						<div class="col">
+							<input type="name" placeholder="Enter your search request here..." class="form-control" style="background-color: #424349;margin-top: 40px;border-color:#535459"><i style=";position: absolute;top:45px;right:25px;font-size: 25px;height: 50px;color:#535479;" class="fa fa-search"></i>
+									
+						</div>
+					</div>
+					<!--end row-->
+				</div>
+				<!--end container-->
+			</div>
+			<!--end container-->
+		</div>
+		<!--end-header-->
+	</a>
+  	<a href="#news">
+		<!--nav-->
+		<nav class="nav p-2 pb-3" >
+			<div class="container">
+				<div class="row" >
+					<div class="col-7">	
+						<ul style="padding:5px 0 0 0">
+							<li>
+								<a style="color:#35ad79;padding: 0;margin:0" id="b" href="#" class="nav-link " data-toggle="dropdown"  aria-haspopup="true" aria-expanded="false"><span class="active-page">Home<i class="fa fa-angle-down "></i></span>
+								</a>
+								    <div class="dropdown-menu">
+										<a class="dropdown-item" href="#">Home Soccer</a>
+										<a class="dropdown-item" href="#">Home Baseball</a>
+										<a class="dropdown-item" href="#">Home Basketball</a>
+										<a class="dropdown-item" href="#">Home Billiards</a>
+										<a class="dropdown-item" href="#">Home Bowling</a>
+										<a class="dropdown-item" href="#">Home Rugby</a>
+									</div>
+							</li>
+							<li>				
+								<a id="o" href="#" class="nav-link " data-toggle="dropdown"  aria-haspopup="true" aria-expanded="false"><span>Features
+								<i class="fa fa-angle-down"></i></span></a>
+								    <div class="dropdown-menu">
+										<a class="dropdown-item" href="#">Customs clearance</a>
+										<a class="dropdown-item" href="#">Storage & Consolidation</a>
+										<a class="dropdown-item" href="#">International Trade & Consulting</a>
+										<a class="dropdown-item" href="#">Sertificates & Permits</a>
+										<a class="dropdown-item" href="#">Insurance</a>
+										<a class="dropdown-item" href="#">Groupage trucking from Europe</a>
+										<a class="dropdown-item" href="#">Delivery from China</a>
+									</div>
+							</li>
+							<li>
+								<a id="o" href="#" class="nav-link " data-toggle="dropdown"  aria-haspopup="true" aria-expanded="false"><span>News<i class="fa fa-angle-down"></i></span></a>
+								    <div class="dropdown-menu">
+										<a class="dropdown-item" href="#">Blog element</a>
+										<a class="dropdown-item" href="#">News 1</a>
+										<a class="dropdown-item" href="#">News 2</a>
+										<a class="dropdown-item" href="#">News 3</a>
+										<a class="dropdown-item" href="#">News 4</a>
+										<a class="dropdown-item" href="#">News 5</a>
+										<a class="dropdown-item" href="#">Blog Post</a>
+									</div>
+							</li>
+							<li>	
+								<a id="o" href="#" class="nav-link"><span>Team<i class="fa fa-angle-down"></i></span></a>
+								<div class="dropdown-menu">
+										<a class="dropdown-item" href="#">Sport Elemetns</a>
+										<a class="dropdown-item" href="#">Game Overview</a>
+										<a class="dropdown-item" href="#">Roster</a>
+										<a class="dropdown-item" href="#">Standings</a>
+										<a class="dropdown-item" href="#">Latest Results</a>
+										<a class="dropdown-item" href="#">Latest Results</a>
+										<a class="dropdown-item" href="#">Game Overview</a>
+										<a class="dropdown-item" href="#">Page Player</a>							
+									</div>
+							</li>
+							<li>
+								<a id="o" href="#" class="nav-link"><span>Shop<i class="fa fa-angle-down"></i></span></a>
+								<div class="dropdown-menu bounceIn">
+										<a class="dropdown-item" href="#">Shop Elemetns</a>
+										<a class="dropdown-item" href="#">Grid shop</a>
+										<a class="dropdown-item" href="#">List shop</a>
+										<a class="dropdown-item" href="#">Fullwidth shop</a>
+										<a class="dropdown-item" href="#">Product Page</a>
+										<a class="dropdown-item" href="#">Shopping card</a>
+										<a class="dropdown-item" href="#">Checkouts</a>							
+									</div>
+							</li>
+						</ul>
+					</div>
+					<div class="col-5">
+						<ul class="social-nav" style="float: right;">
+							<li><i id="l" class="fa fa-facebook-f"></i></li>
+							<li><i id="l" class="fa fa-twitter"></i></li>
+							<li><i id="l" class="fa fa-google-plus-g"></i></li>
+							<li><i id="l" class="fa fa-instagram"></i></li>
+						</ul>
+					</div>
+					<!--end col-->
+				</div>
+				<!--end row-->
+			</div>
+			<!--end container-->
+		</nav>
+		<!--end nav-->
+    </a>
+    <div class="row">
+    	
+		  	<a style="font-size:15px;position: absolute;top:2px;left:15px;" id="icon" class="icon" data-id="1" onclick="myChange()">
+		  		<div class="bar1"></div>
+				<div class="bar2"></div>
+				<div class="bar3"></div>
+		  		
+		  	</a>
+			<div id="displayShopping" style="position: absolute;top:-10px;right: 20px;">
+			<span style="float:right;padding-top:12px;">
+				<ul>
+					<li style="border-right: 1px solid #35363c;padding:0 10px;">	
+						<button class="btn btn-outline-success pl-1 mb-1" style="font-size: 14px;width: 40px">En<i class="fa fa-angle-down pl-1"></i></button>
+					</li>
+					<li style="border-right: 1px solid #35363c;padding:0 10px;">	
+						<i style="color:#d8cf50;font-size: 20px;" class="fa fa-shopping-cart"></i>
+						<span style="background-color: #35ad79;padding:2px 6px;font-weight: bold;border-radius: 50%;color:white;font-size: 12px">2</span>
+					</li>
+					<li style="padding:0 10px;">
+						<i style="color:#d8cf50;font-size: 20px;margin-right:5px;" class="fa fa-sign-in"></i>
+											
+					</li>
+				</ul>
+			</span>
+		</div>
+	<!--end col-->  	
+	</div>
+  	<a>
+		<div id="displayShopping" style="position: absolute;top:-5px;right: 20px;">
+			<span style="float:right;padding-top:12px;">
+				<ul>
+					<li style="border-right: 1px solid #35363c;padding:0 10px;">	
+						<button class="btn btn-outline-success pl-1 mb-1" style="font-size: 14px;width: 40px">En<i class="fa fa-angle-down pl-1"></i></button>
+					</li>
+					<li style="border-right: 1px solid #35363c;padding:0 10px;">	
+						<i style="color:#d8cf50;font-size: 20px;" class="fa fa-shopping-cart"></i>
+						<span style="background-color: #35ad79;padding:2px 6px;font-weight: bold;border-radius: 50%;color:white;font-size: 12px">2</span>
+					</li>
+					<li style="padding:0 10px;">
+						<i style="color:#d8cf50;font-size: 20px;margin-right:5px;" class="fa fa-sign-in"></i>
+											
+					</li>
+				</ul>
+			</span>
+		</div>
+	<!--end col-->
+	</a>
+</div>
