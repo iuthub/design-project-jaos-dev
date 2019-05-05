ad6885cfa4acf7bef19f3741482cce666c31c982
Safarmurod <s.musulmonov@student.inha.uz>
2019-05-05 15:03:15 +0500

    factories and seeds add

--- TicketBooking/database/seeds/DatabaseSeeder.php
+++ TicketBooking/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(RoleTableSeeder::class);
+        $this->call(UserTableSeeder::class);
+        $this->call(UserDetailTableSeeder::class);
+        $this->call(CategoryTableSeeder::class);
+        $this->call(ImagesTableSeeder::class);
+        $this->call(ProductTableSeeder::class);
+        $this->call(BasketTableSeeder::class);
+        $this->call(BasketProductsTableSeeder::class);
+        $this->call(OrderTableSeeder::class);
     }
 }
