c9889a2da2a48da672e8e16ce090bc10dfda608c
JavokhirBuvanazarov <j.buvanazarov1999@gmail.com>
2019-05-05 15:42:20 +0500

    Admin views user views

--- TicketBooking/resources/views/layouts/main.blade.php
+++ TicketBooking/resources/views/layouts/main.blade.php
@@ -1,99 +1,267 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>E-store - {{ config('app.name') }} </title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/styles/bootstrap4/bootstrap.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap-select.min.css') }}">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote-bs4.css" rel="stylesheet"
+          type="text/css">
+    <link href="{{ asset('assets/plugins/font-awesome-4.7.0/css/font-awesome.min.css')}}" rel="stylesheet"
+          type="text/css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/plugins/OwlCarousel2-2.2.1/owl.carousel.css') }}">
+    <link rel="stylesheet" type="text/css"
+          href="{{ asset('assets/plugins/OwlCarousel2-2.2.1/owl.theme.default.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/plugins/OwlCarousel2-2.2.1/animate.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/styles/main_styles.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/styles/responsive.css') }}">
+    <link href="{{asset("assets/styles/toastr.min.css")}}" rel="stylesheet">
 
-        <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    @yield('css')
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <script>
+        window.csrfToken = "{{ csrf_token() }}"
+    </script>
+</head>
 
-            .full-height {
-                height: 100vh;
-            }
+<body data-status="{{Session::get("status")}}">
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+<div class="super_container">
 
-            .position-ref {
-                position: relative;
-            }
+    <!-- Header -->
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+    <header class="header trans_300">
 
-            .content {
-                text-align: center;
-            }
+        <!-- Top Navigation -->
 
-            .title {
-                font-size: 84px;
-            }
+        <div class="top_nav">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="top_nav_left">Favourite brands and hottest trends</div>
+                    </div>
+                    <div class="col-md-6 text-right">
+                        <div class="top_nav_right">
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+                            <ul class="top_nav_menu">
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+                                <!-- Currency / Language / My Account -->
+                                @if(Auth::guest())
+                                    <li class="language">
+                                        <a href="{{ route('login') }}"><i class="fa fa-sign-in" aria-hidden="true"></i>
+                                            Sign In</a>
+                                    </li>
+                                    <li class="language">
+                                        <a href="{{ route('register') }}"><i class="fa fa-user-plus"
+                                                                             aria-hidden="true"></i> Register</a>
+                                    </li>
+                                @else
+                                    <li class="account">
+                                        <a href="#">
+                                            {{ Auth::user()->name }}
+                                            <i class="fa fa-angle-down"></i>
+                                        </a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+                                        <ul class="account_selection">
+                                            @if(Auth::user()->isItAuthorized("admin"))
+                                                <li><b>ADMIN</b></li>
+                                                <li><a href="{{ url('/admin-users') }}"><i
+                                                                class="fa fa-btn fa-users"></i>Users</a>
+                                                </li>
+                                                <li><a href="{{ url('/admin-category') }}"><i
+                                                                class="fa fa-btn fa-list-ul"></i>Category</a></li>
+                                                <li><a href="{{ url('/admin-products') }}"><i
+                                                                class="fa fa-btn fa-cubes"></i>Products</a>
+                                                </li>
+                                                <li><a href="{{ url('/admin-orders') }}"><i
+                                                                class="fa fa-btn fa-cogs"></i>Orders</a></li>
+                                                <li class="divider"></li>
+                                            @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel Yes!
-                </div>
+                                            @if(Auth::user())
+                                                <li><b>USER</b></li>
+                                                <li><a href="{{ url('/profile') }}"><i class="fa fa-btn fa-user"></i>Profile</a>
+                                                </li>
+                                                <li><a href="{{ url('/orders') }}"><i class="fa fa-btn fa-list-alt"></i>Orders</a>
+                                                </li>
+                                            @endif
+                                            <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a>
+                                            </li>
+                                        </ul>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+
+                                    </li>
+                                @endif
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </body>
+
+        <!-- Main Navigation -->
+
+        <div class="main_nav_container">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 text-right">
+                        <div class="logo_container">
+                            <a href="/"><span>E-store</span></a>
+                        </div>
+                        <nav class="navbar">
+                            <ul class="navbar_menu">
+                                <li><a href="/">HOMEPAGE</a></li>
+
+                                @foreach($categoryMenu as $menu)
+                                    <li><a href="/category/{{ $menu->slug }}">{{ $menu->category_name }}</a>
+                                    </li>
+                                @endforeach
+
+                                <li><a href="{{ route('contact') }}">CONTACT</a></li>
+
+                            </ul>
+                            <ul class="navbar_user">
+
+                                <li class="checkout">
+                                    <a href="{{route('basket')}}">
+                                        <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                                        <span id="checkout_items" class="checkout_items">{{ Cart::count() }}</span>
+                                    </a>
+                                </li>
+                            </ul>
+                            <div class="hamburger_container">
+                                <i class="fa fa-bars" aria-hidden="true"></i>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </header>
+
+    <div class="fs_menu_overlay"></div>
+    <div class="hamburger_menu">
+        <div class="hamburger_close"><i class="fa fa-times" aria-hidden="true"></i></div>
+        <div class="hamburger_menu_content text-right">
+            <ul class="menu_top_nav">
+                <li class="menu_item"><a href="{{ route('home') }}">HOMEPAGE</a></li>
+                @foreach($categoryMenu as $menu)
+                    <li class="menu_item"><a href="/category/{{ $menu->slug }}">{{ $menu->category_name }}</a>
+                    </li>
+                @endforeach
+                <li class="menu_item"><a href="{{ route('contact') }}">CONTACT</a></li>
+
+
+                @if(Auth::guest())
+                    <li class="menu_item">
+                        <a href="{{ route('login') }}"><i class="fa fa-sign-in" aria-hidden="true"></i>
+                            Sign In</a>
+                    </li>
+                    <li class="menu_item">
+                        <a href="{{ route('register') }}"><i class="fa fa-user-plus"
+                                                             aria-hidden="true"></i> Register</a>
+                    </li>
+                @else
+                    <li class="menu_item has-children">
+                        <a href="#">
+                            {{ Auth::user()->name }} {{ Auth::user()->surname}}
+                            <i class="fa fa-angle-down"></i>
+                        </a>
+
+                        <ul class="menu_selection">
+                            @if(Auth::user()->isItAuthorized("admin"))
+                                <li><b>ADMIN</b></li>
+                                <li><a href="{{ url('/admin-users') }}">Users</a></li>
+                                <li><a href="{{ url('/admin-category') }}">Category</a></li>
+                                <li><a href="{{ url('/admin-products') }}">Products</a>
+                                </li>
+                                <li><a href="{{ url('/admin-orders') }}">Orders</a></li>
+                            @endif
+
+                            @if(Auth::user())
+                                <li><b>USER</b></li>
+                                <li><a href="{{ url('/profile') }}"><i class="fa fa-btn fa-user"></i>Profile</a></li>
+                                <li><a href="{{ url('/orders') }}"><i class="fa fa-btn fa-list-alt"></i>Orders</a></li>
+                            @endif
+                            <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a>
+                            </li>
+                        </ul>
+
+
+                    </li>
+                @endif
+
+
+            </ul>
+        </div>
+    </div>
+
+
+@yield('content')
+
+<!-- Footer -->
+
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6"></div>
+                <div class="col-lg-6">
+                    <div class="footer_social d-flex flex-row align-items-center justify-content-lg-end justify-content-center">
+                        <ul>
+                            <li><a href="http://www.facebook.com/"><i class="fa fa-facebook"
+                                                                                   aria-hidden="true"></i></a>
+                            </li>
+                            <li><a href="http://www.twitter.com/"><i class="fa fa-twitter"
+                                                                               aria-hidden="true"></i></a>
+                            </li>
+                            <li><a href="http://www.instagram.com/"><i class="fa fa-instagram"
+                                                                                aria-hidden="true"></i></a></li>
+                            <li><a href="http://www.github.com/"><i class="fa fa-github"
+                                                                             aria-hidden="true"></i></a></li>
+                            <li><a href="http://www.gitlab.com/"><i class="fa fa-gitlab"
+                                                                             aria-hidden="true"></i></a></li>
+                            <li><a href="#"><i class="fa fa-skype" aria-hidden="true"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="footer_nav_container">
+                        <div class="cr">©2019 All Rights Reserved. This template is made with <i class="fa fa-heart-o"
+                                                                                                 aria-hidden="true"></i>
+                            by <a href="#">JAOS</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+</div>
+
+
+<script src="{{ asset('assets/js/jquery-3.2.1.min.js') }}"></script>
+<script src="{{ asset('assets/styles/bootstrap4/popper.js') }}"></script>
+<script src="{{ asset('assets/styles/bootstrap4/bootstrap.min.js') }}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote-bs4.js"></script>
+<script src="{{ asset('assets/js/bootstrap-select.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/Isotope/isotope.pkgd.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/OwlCarousel2-2.2.1/owl.carousel.js') }}"></script>
+<script src="{{ asset('assets/plugins/easing/easing.js') }}"></script>
+<script src="{{ asset('js/custom.js') }}"></script>
+<script src="{{ asset('assets/js/custom.js') }}"></script>
+<script src="{{asset("assets/js//toastr.min.js")}}"></script>
+
+@yield('js')
+
+
+</body>
+
 </html>
